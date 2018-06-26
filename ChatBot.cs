fdc292de88c77d98afc7b7f0210c973fe8fb6f43
Anolog <stephen.kostiw@carleton.ca>
2018-06-26 12:37:44 -0400

    Added more commands for mods

--- ChatBot.cs
+++ ChatBot.cs
@@ -18,7 +18,7 @@ namespace TwitchBot
 
         public ChatBot()
         {
-            m_CurrentMonkaCount = 100;
+            m_CurrentMonkaCount = 20;
         }
 
         internal void Connect()
@@ -48,9 +48,160 @@ namespace TwitchBot
                 m_Client.ChatThrottler.Clear();
             }
 
-            if (e.ChatMessage.Message.StartsWith("!monkaCount"))
+            //Check the current amount of monkaS
+            if (e.ChatMessage.Message.StartsWith("!MonkaCount"))
             {
                 m_Client.SendMessage(TwitchInfo.ChannelName, "Available MonkaS: " + m_CurrentMonkaCount);
+                return;
+            }
+
+            //This code is for adding more monkaS stock
+            if (e.ChatMessage.Message.StartsWith("!MonkaAdd") && e.ChatMessage.IsModerator == true|| e.ChatMessage.IsBroadcaster == true && e.ChatMessage.Message.StartsWith("!MonkaAdd"))
+            {
+                string[] chatMessage = e.ChatMessage.Message.Split(' ', '\t');
+
+                if (chatMessage.Length < 1)
+                {
+                    Console.WriteLine("Command did not recieve a value");
+                    return;
+                }
+
+                else
+                {
+                    if (chatMessage[1] == null)
+                    {
+                        Console.WriteLine("Null value hit in chatMessage[1]");
+                        return;
+                    }
+
+                    int val;
+
+                    if (Int32.TryParse(chatMessage[1], out val))
+                    {
+                        Console.WriteLine("monkaS Val increased by mod: " + e.ChatMessage.Username + " by value: " + val);
+                        m_CurrentMonkaCount += val;
+
+                        m_CurrentMonkaCount--;
+                        m_Client.SendMessage(TwitchInfo.ChannelName, e.ChatMessage.Username + " has increased the monkaS count! There are " + m_CurrentMonkaCount + " left!");
+                        return;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Error with amount to increase: " + chatMessage[1]);
+                        return;
+                    }
+                }
+            }
+
+            //Get rid of monkaS
+            if (e.ChatMessage.Message.StartsWith("!MonkaRemove") && e.ChatMessage.IsModerator == true || e.ChatMessage.IsBroadcaster == true && e.ChatMessage.Message.StartsWith("!MonkaRemove"))
+            {
+                string[] chatMessage = e.ChatMessage.Message.Split(' ', '\t');
+
+                if (chatMessage.Length < 1)
+                {
+                    Console.WriteLine("Command did not recieve a value");
+                    return;
+                }
+
+                else
+                {
+                    if (chatMessage[1] == null)
+                    {
+                        Console.WriteLine("Null value hit in chatMessage[1]");
+                        return;
+                    }
+
+                    int val;
+
+                    if (Int32.TryParse(chatMessage[1], out val))
+                    {
+                        Console.WriteLine("monkaS Val decreased by mod: " + e.ChatMessage.Username + " by value: " + val);
+                        m_CurrentMonkaCount -= val;
+
+                        if (m_CurrentMonkaCount <= 0)
+                        {
+                            //1 for the bot to use
+                            m_CurrentMonkaCount = 1;
+                        }
+
+                        m_CurrentMonkaCount--;
+                        m_Client.SendMessage(TwitchInfo.ChannelName, e.ChatMessage.Username + " has decreased the monkaS count! There are " + m_CurrentMonkaCount + " left!");
+                        return;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Error with amount to decrease: " + chatMessage[1]);
+                        return;
+                    }
+                }
+            }
+
+            //Let mods set the amount of monkaS
+            if (e.ChatMessage.Message.StartsWith("!MonkaChange") && e.ChatMessage.IsModerator == true || e.ChatMessage.IsBroadcaster == true && e.ChatMessage.Message.StartsWith("!MonkaChange"))
+            {
+                string[] chatMessage = e.ChatMessage.Message.Split(' ', '\t');
+
+                if (chatMessage.Length < 1)
+                {
+                    Console.WriteLine("Command did not recieve a value");
+                    return;
+                }
+
+                else
+                {
+                    if (chatMessage[1] == null)
+                    {
+                        Console.WriteLine("Null value hit in chatMessage[1]");
+                        return;
+                    }
+
+                    int val;
+
+                    if (Int32.TryParse(chatMessage[1], out val))
+                    {
+                        Console.WriteLine("monkaS Val changed by mod: " + e.ChatMessage.Username + " by value: " + val);
+                        m_CurrentMonkaCount = val;
+
+                        if (m_CurrentMonkaCount <= 0)
+                        {
+                            //1 for the bot to use
+                            m_CurrentMonkaCount = 1;
+                        }
+
+                        m_CurrentMonkaCount--;
+                        m_Client.SendMessage(TwitchInfo.ChannelName, e.ChatMessage.Username + " has changed the monkaS count! There are " + m_CurrentMonkaCount + " left!");
+                        return;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Error with amount to decrease: " + chatMessage[1]);
+                        return;
+                    }
+                }
+            }
+
+            if (e.ChatMessage.Message.StartsWith("!MonkaCommands") && e.ChatMessage.IsModerator == true || e.ChatMessage.IsBroadcaster == true && e.ChatMessage.Message.StartsWith("!MonkaCommands"))
+            {
+                Console.WriteLine("Moderator: " + e.ChatMessage.Username + " asked what the commands were");
+
+                m_Client.SendWhisper(e.ChatMessage.Username, e.ChatMessage.Username + " the commands are: !MonkaCount to view the current monka left, !MonkaAdd [number] to add more monka, !MonkaRemove [number] to remove that amount of monka, and !MonkaChange [number] to change the amount of monka left."
+                + " Feel free to PM Piemeup on Twitch or Anolog#6680 on Discord for any questions!");
+
+                return;
+            }
+
+            if (e.ChatMessage.Message.StartsWith("!MonkaCommands") && e.ChatMessage.IsModerator == false || e.ChatMessage.IsBroadcaster == false && e.ChatMessage.Message.StartsWith("!MonkaCommands"))
+            {
+                Console.WriteLine("Non mod: " + e.ChatMessage.Username + " asked what the commands were");
+
+                m_Client.SendWhisper(e.ChatMessage.Username, e.ChatMessage.Username + " the commands for non mods are: !MonkaCount to view the current monka left."
+                + " Feel free to PM Piemeup on Twitch or Anolog#6680 on Discord for any questions!");
+
+                return;
             }
 
             else
@@ -116,7 +267,7 @@ namespace TwitchBot
                             m_CurrentMonkaCount = 0;
                             m_Client.SendMessage(TwitchInfo.ChannelName, "WE ARE OUT OF MONKAS, SPAM MORE TriHard !");
                             m_Client.TimeoutUser(e.ChatMessage.Username, TimeSpan.FromSeconds(1));
-                        }          
+                        }
                     }
 
                     else if (chatMessage[i] == "TriHard")
